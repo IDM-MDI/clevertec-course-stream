c37f276ca48f0ba763a6fc29d3dfec87cd66a7d4
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-02-12 00:22:35 +0300

    task 14 successfully complete

---
+++ src/main/java/by/sologub/model/ModelFieldName.java
@@ -0,0 +1,12 @@
+package by.sologub.model;
+
+public class ModelFieldName {
+    private ModelFieldName() {}
+    public static final String OCEANIA = "Oceania";
+    public static final String MALE = "Male";
+    public static final String FEMALE = "Female";
+    public static final String HUNGARIAN = "Hungarian";
+    public static final String JAPANESE = "Japanese";
+    public static final String INDONESIAN = "Indonesian";
+    public static final String HOSPITAL = "Hospital";
+}
--- src/main/java/by/sologub/validator/AgeValidator.java
+++ src/main/java/by/sologub/validator/AgeValidator.java
@@ -1,8 +1,12 @@
 package by.sologub.validator;
 
+import by.sologub.model.Person;
 import by.sologub.util.DateCalculator;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
+import static by.sologub.model.ModelFieldName.FEMALE;
 
 public class AgeValidator {
     private AgeValidator() {}
@@ -11,4 +15,20 @@ public class AgeValidator {
         int age = DateCalculator.calculateAge(birthdate);
         return age > 18 && age < 30;
     }
+    public static boolean isPersonEvacuationValid(Person person) {
+        return !isPersonAdult(person) || isPersonPensionAge(person);
+    }
+
+    public static boolean isPersonPensionAge(Person person) {
+        if(Objects.isNull(person)) {
+            return false;
+        }
+        int age = DateCalculator.calculateAge(person.getDateOfBirth());
+        boolean isFemale = person.getGender().equals(FEMALE);
+        return isFemale ? age > 58 : age > 63;
+    }
+    public static boolean isPersonAdult(Person person) {
+        return Objects.nonNull(person) && DateCalculator.calculateAge(person.getDateOfBirth()) > 18;
+    }
+
 }
